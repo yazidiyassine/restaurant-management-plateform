fcda6542da345b85f95cdd57ae21325babf39462
Yassine Yazidi <104203113+yazidiyassine@users.noreply.github.com>
2023-07-01 22:26:29 +0100

    adding the first entity + project struct

---
+++ src/main/java/com/rms/model/User.java
@@ -0,0 +1,39 @@
+package com.rms.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.io.Serializable;
+
+@Entity
+@DynamicUpdate
+@DynamicInsert
+@Table(name = "user")
+@Data
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "role")
+    private String role;
+
+}
